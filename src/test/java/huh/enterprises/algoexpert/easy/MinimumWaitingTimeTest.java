aaa96e5d7140115cbd17bcac2b49a278c763d122
Jun Huh <junhuhdev@gmail.com>
2022-05-11 21:36:29 +0200

    Update MinimumWaitingTimeTest.java

--- src/test/java/huh/enterprises/algoexpert/easy/MinimumWaitingTimeTest.java
+++ src/test/java/huh/enterprises/algoexpert/easy/MinimumWaitingTimeTest.java
@@ -2,6 +2,11 @@ package huh.enterprises.algoexpert.easy;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MinimumWaitingTimeTest {
@@ -13,7 +18,21 @@ public class MinimumWaitingTimeTest {
     }
 
     public int minimumWaitingTime(int[] queries) {
+        if (queries.length == 0 || queries.length == 1) {
+            return 0;
+        }
+        // Sort the array descending
+        Integer[] values = Arrays.stream(queries).boxed().toArray(Integer[]::new);
+        Arrays.sort(values);
+        int min = 0;
+        List<Integer> sum = new ArrayList<>();
+        for (int i = 0; i < values.length; i++) {
+            if (i != 0) {
+                min += values[i - 1];
+                sum.add(min);
+            }
+        }
         // Write your code here.
-        return -1;
+        return sum.stream().mapToInt(Integer::intValue).sum();
     }
 }
